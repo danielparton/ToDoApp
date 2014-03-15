dec67ba554d77d11da9ad19c5342262ac280d319
Daniel L. Parton <Danny.Parton@gmail.com>
2014-03-15 18:27:11 -0400

    Added Python/Flask functions for adding and deleting todo-list entries.

--- README.md
+++ README.md
@@ -3,7 +3,33 @@ webdev
 
 For testing web development tools
 
-Manifest
---------
+## Manifest
 
 flask/  - for testing Flask microframework
+
+## todo-list application
+
+Flask backend
+MySQL database
+
+### Operation:
+
+Leave this running:
+
+    ./flask/todo.py
+
+To get list of tasks from browser, enter:
+
+    http://localhost:5000/todo/tasks
+
+From curl, enter:
+
+    curl -i http://localhost:5000/todo/tasks
+
+To add a task, enter (for example):
+
+    curl -i -H "Content-Type: application/json" -X POST -d '{"title":"Learn Flask"}' http://localhost:5000/todo/tasks
+
+To delete a task by task ID (defined in the URL) (for example):
+
+    curl -i -X DELETE http://localhost:5000/todo/tasks/2
--- flask/todo.py
+++ flask/todo.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
@@ -7,8 +7,14 @@ import MySQLdb as mdb
 
 connection = mdb.connect('localhost', 'DLPtest', 'DLPtest', 'python_test')
 
+
+# ======
+# Get list of tasks
+# ======
+
 @app.route('/todo/tasks', methods = ['GET'])
 def get_tasks():
+    '''Return all tasks in JSON format'''
 
     with connection:
         cursor = connection.cursor()
@@ -18,6 +24,60 @@ def get_tasks():
     tasks = [row for row in cursor]
 
     return jsonify( {'tasks': tasks} )
+
+
+# ======
+# Add a task
+# ======
+
+@app.route('/todo/tasks', methods = ['POST'])
+def create_task():
+    '''Accepts a task in JSON format and adds it to the SQL DB.
+    Returns the added task.'''
+
+    # request must be json format
+    if not request.json or not 'title' in request.json:
+        abort(400)
+
+    # get title and description from the json request
+    title = request.json['title']
+    description = request.json.get('description', "")
+
+    with connection:
+        cursor = connection.cursor()
+
+    # insert task into the SQL DB
+    cursor.execute("INSERT INTO todo_list(title, description) VALUES('%s', '%s')" % (title, description))
+
+    # return the row just added
+    cursor.execute("SELECT * FROM todo_list ORDER BY Id DESC LIMIT 1")
+    added_task = [row for row in cursor]
+
+    return jsonify( { 'task': added_task } ), 201
+
+
+# ======
+# Delete a task
+# ======
+
+@app.route('/todo/tasks/<int:task_id>', methods = ['DELETE'])
+def delete_task(task_id):
+    '''Delete a task.'''
+
+    with connection:
+        cursor = connection.cursor()
+
+    cursor.execute("DELETE FROM todo_list WHERE Id = '%d'" % task_id)
+
+    return jsonify( { 'result': True } )
+
+
+# ======
+# Toggle task complete field
+# ======
+
+# TODO
+
 
 if __name__ == '__main__':
     app.run(debug = True)
