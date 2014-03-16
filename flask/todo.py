a953007638bf3cf47f1b1d55a671ef4c17d10791
EC2 Default User <ec2-user@ip-10-179-14-59.ec2.internal>
2014-03-16 16:33:37 +0000

    Completed todo backend.

--- flask/todo.py
+++ flask/todo.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, abort, make_response
 
 app = Flask(__name__)
 
@@ -7,6 +7,14 @@ import MySQLdb as mdb
 
 connection = mdb.connect('localhost', 'DLPtest', 'DLPtest', 'python_test')
 
+
+# ======
+# 404 error handler
+# ======
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify( { 'error': 'Not found' } ), 404)
 
 # ======
 # Get list of tasks
@@ -24,6 +32,26 @@ def get_tasks():
     tasks = [row for row in cursor]
 
     return jsonify( {'tasks': tasks} )
+
+
+# ======
+# Get a single task
+# ======
+
+@app.route('/todo/tasks/<int:task_id>', methods = ['GET'])
+def get_task(task_id):
+    '''Return a single task in JSON format'''
+
+    with connection:
+        cursor = connection.cursor()
+
+    cursor = connection.cursor(mdb.cursors.DictCursor)
+    cursor.execute("SELECT * FROM todo_list WHERE Id = %d" % task_id)
+    task = [row for row in cursor]
+    if len(task) == 0:
+        abort(404)
+
+    return jsonify( {'task': task} )
 
 
 # ======
@@ -76,9 +104,40 @@ def delete_task(task_id):
 # Toggle task complete field
 # ======
 
-# TODO
+@app.route('/todo/tasks/<int:task_id>', methods = ['PUT'])
+def update_task(task_id):
+    with connection:
+        cursor = connection.cursor()
+
+    cursor = connection.cursor(mdb.cursors.DictCursor)
+    cursor.execute("SELECT * FROM todo_list WHERE Id = %d" % task_id) # use LIMIT to limit range of rows returned
+    task = [row for row in cursor]
+
+    if len(task) == 0:
+        abort(404)
+    if not request.json:
+        abort(400)
+    if 'title' in request.json and type(request.json['title']) != unicode:
+        abort(400)
+    if 'description' in request.json and type(request.json['description']) is not unicode:
+        abort(400)
+    if 'complete' in request.json and type(request.json['complete']) is not int:
+        abort(400)
+
+    task[0]['title'] = request.json.get('title', task[0]['title'])
+    task[0]['description'] = request.json.get('description', task[0]['description'])
+    task[0]['complete'] = request.json.get('complete', task[0]['complete'])
+
+    cursor.execute("UPDATE todo_list SET title='%s', description='%s', complete='%d' WHERE Id='%d'" % (task[0]['title'], task[0]['description'], task[0]['complete'], task_id))
+
+    # return the row just updated
+    cursor.execute("SELECT * FROM todo_list WHERE Id = %d" % task_id)
+    updated_task = [row for row in cursor]
+
+    return jsonify( { 'task': updated_task } )
+    #return jsonify( { 'task': task } )
 
 
 if __name__ == '__main__':
-    app.run(debug = True)
+    app.run(host='0.0.0.0', debug=True)
 
